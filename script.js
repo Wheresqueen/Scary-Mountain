39789157c5b06c3e66da1d5cf392081e2ecee260
Wheresqueen <127963318+Wheresqueen@users.noreply.github.com>
2023-03-15 09:20:33 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let button1 = document.querySelector(".option-one");
+let optionOneScreen = document.querySelector(".option-one-screen");
+
+let button2 = document.querySelector(".option-one");
+ let optionTwoScreen = document.querySelector("option-two-screen");
+
+let button3 =document.querySelector(".option-three");
+let opionThreeScreen=document.querySelector(".option-three-screen");
+
+let button4= document.querySelector("option-four");
+let optionFourScreen=document.querySelector("option-four-screen");
+
+
+
+
+
+
